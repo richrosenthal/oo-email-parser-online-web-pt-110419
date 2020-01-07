feac7f62734236b95a66f8f02cf94cb44c261728
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-01-07 17:32:47 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -13,7 +13,7 @@ class EmailAddressParser
   
   def parse
   name.match(/\b[A-Z0-9._%a-z-]+@(?:[A-Z0-9a-z-]+.)+[A-Za-z]{2,4}\z/)
-  array = name.split(', ')
+  array = name.split(/,\s/)
   end
   
 end
